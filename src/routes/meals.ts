d208d34c7f1d18154cc83fb1ec94bff929ec6e16
Pedro Italo BC <pedroitalobc@gmail.com>
2023-04-21 13:31:57 -0300

    added update route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -78,4 +78,47 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(202).send()
     },
   )
+
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const updateMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const editMealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().default('').optional(),
+        inDiet: z.enum(['diet', 'notDiet']).optional(),
+        dateAndHour: z
+          .string()
+          .regex(/(^[0-9]{4}[-][0-9]{2}[-][0-9]{2}[ ][0-9]{2}[:][0-9]{2}$)/)
+          .optional(),
+      })
+
+      const { id } = updateMealParamsSchema.parse(request.params)
+      const { dateAndHour, description, inDiet, name } =
+        editMealBodySchema.parse(request.body)
+
+      const { sessionId } = request.cookies
+
+      const updatedAt = dayjs(new Date()).format('YYYY/MM/DD HH:mm:ss')
+
+      await knex('meals')
+        .where({
+          session_id: sessionId,
+          id,
+        })
+        .update({
+          in_diet: inDiet,
+          name,
+          date_and_hour: dateAndHour,
+          description,
+          updated_at: updatedAt,
+        })
+
+      return reply.status(200).send()
+    },
+  )
 }
